7485f377f24598ac08d2d03616cd5741c14f38ad
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2024-04-11 23:14:28 +0530

    told server location

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ class DjangoServerControl(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Django Server Control")
-        self.geometry("200x100")
+        self.geometry("200x150")
         self.server_process = None
 
         self.start_button = tk.Button(
@@ -19,6 +19,11 @@ class DjangoServerControl(tk.Tk):
             self, text="Stop Server", command=self.stop_server, state=tk.DISABLED
         )
         self.stop_button.pack(pady=10)
+
+        # tell that server will start at localhost 8000
+        self.label = tk.Label(self, text="Server will start at localhost:8000")
+        self.label.pack(pady=10)
+
     def start_server(self):
         if self.server_process is None:
             os.chdir("PCUsageAnalyzer")
@@ -39,7 +44,7 @@ class DjangoServerControl(tk.Tk):
         self.destroy()
         # exit the main program
         os._exit(0)
-        
+
 
 if __name__ == "__main__":
     app = DjangoServerControl()
